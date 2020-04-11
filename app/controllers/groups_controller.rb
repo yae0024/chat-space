f721bbd13445fc73e1dbe1e2a7aaae262e7b92c7
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-04-11 17:11:02 +0900

    Set index / edit / update action in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,25 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
   end
 
   def create
   end
-  
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
