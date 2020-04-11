fdd74a107e9d0820d7f35604384720fa35ccbfc0
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-04-11 17:32:05 +0900

    Define new and create action in group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,9 +4,17 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
