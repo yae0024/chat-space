a9ddf768fefba61ca836619dfd8eaacbdb6dd2d4
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-04-21 21:42:29 +0900

    Describe the processing when saving the message succeeds and fails

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -50,4 +50,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.submit_btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   });
+});
