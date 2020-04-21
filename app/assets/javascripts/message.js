fc6110d9e512109dcc05b17935261b7af1e59bb2
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-04-21 21:40:13 +0900

    Write the html code to add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,43 @@
 $(function(){
   function buildHTML(message){
+    if (message.image) {
+      var html = `<div class='message'>
+                    <div class='message__first_line'>
+                      <p class='message__first_line__user_name'>
+                        ${message.user_name}
+                      </p>
+                      <p class='message__first_line__date'>
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <div class='message__second_line'>
+                      <p>
+                        ${message.body}
+                      </p>
+                      <img class="message__second_line__image" src=${message.image}>
+                    </div>
+                  </div>`
+                  return html;
+    } else {
+      var html = `<div class='message'>
+                    <div class='message__first_line'>
+                      <p class='message__first_line__user_name'>
+                        ${message.user_name}
+                      </p>
+                      <p class='message__first_line__date'>
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <div class='message__second_line'>
+                      <p>
+                        ${message.body}
+                      </p>
+                    </div>
+                  </div>`
+                  return html;
+    };
+  }
+
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
