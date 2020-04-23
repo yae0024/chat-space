491b62d21cab2a2ad9a1464f845cd5104b70dda5
yae0024 <62417944+yae0024@users.noreply.github.com>
2020-04-23 16:49:22 +0900

    Send last_message_id to controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,5 +62,12 @@ $(function(){
     });
   });
   var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
   }
 });
